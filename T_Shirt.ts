9bb64d302f2cd66ba745ad9d396460a22b510957
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:43:26 -0800

    Add files via upload

---
+++ T_Shirt.ts
@@ -0,0 +1,9 @@
+//Written by : TAYYABA RAO
+//Date : 18/feb/2024
+//Roll no: 00388416
+
+function make_shirt(size: string, message:string):void{
+    console.log(`Creating a ${size} shirt with the message: "${message}`);
+}
+
+make_shirt('large', 'Hello world!');
